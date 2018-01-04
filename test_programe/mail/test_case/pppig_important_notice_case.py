525a874b7d22902eccaf765519e0d5af609d7936
Administrator <2513953126@qq.com>
2018-01-04 17:50:57 +0800

    closenotice

--- test_programe/mail/test_case/pppig_important_notice_case.py
+++ test_programe/mail/test_case/pppig_important_notice_case.py
@@ -3,7 +3,7 @@
 from time import sleep
 import unittest, random, sys
 from model import myunit, function
-from page_object.pppigimportant_notice_page import Important_noticePage
+from page_object.pppig_important_notice_page import Important_noticePage
 from page_object.mail_page import MailPage
 
 class Important_noticeTest(myunit.MyTest):
@@ -11,7 +11,7 @@ class Important_noticeTest(myunit.MyTest):
 	def test_important_notice_close(self):
 		"""点击关闭重要通知"""
 		po = Important_noticePage(self.driver)
-		# sleep(2)
+		sleep(2)
 		po.open()
 		sleep(2)
 		po.pppignotice_Action()
